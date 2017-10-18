c298334b3ef6b3698361fee80c63608e690835fd
zaim ramlan <zaem.muz@gmail.com>
2017-10-18 22:27:03 +0800

    initial commit until form creation

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :ideas
+
+  resources :students, :only => [:index]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
